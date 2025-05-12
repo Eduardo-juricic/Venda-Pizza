1cf01f99110fd7b3d60c02c8371640466eade3f7
Eduardo Juricic <edujuricic@gmail.com>
2025-05-12 19:45:36 -0300

    Removida pasta backend (removendo arquivos desnecessários)

--- backend/generate-hash.js
+++
@@ -1,9 +0,0 @@
-const bcrypt = require("bcrypt");
-
-async function generateHash(password) {
-  const saltRounds = 10;
-  const hash = await bcrypt.hash(password, saltRounds);
-  console.log("Hash da senha:", hash);
-}
-
-generateHash("211204"); // Substitua 'suaSenhaAdmin' pela senha desejada
